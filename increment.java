531072974190056f549d2eb40e33cffbf3b8ba69
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-06-13 20:55:36 +0530

    increment,for
    while,degrement

---
+++ increment.java
@@ -0,0 +1,69 @@
+package bharath;
+
+public class increment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		int no1=5;
+//		System.out.println(no1++);//post increment
+//		System.out.println(no1);
+//		System.out.println(++no1);//pre increment
+//		int a=5,b=10;
+//		System.out.println(a++ + b++);//15
+//		System.out.println(a+b);//17
+//		System.out.println(a++ + b++);//17
+//		System.out.println(a+b);//19
+//		System.out.println(a-- + b--);//15
+//		System.out.println(a+b);//13
+//		System.out.println(a-- + b--);//13
+//		System.out.println(a+b);//11
+//		System.out.println(++a + a++ + ++a + a++);
+						//(6+6+8+8)
+//		System.out.println(++a + ++a + ++a + ++a);
+						//(6+7+8+9)
+//		int c=10,d=20;
+//		System.out.println(d++ - c++ + (--d + --c));
+						//(20 -10 +(20+10))
+
+//		int barath=0;//2
+//		while(barath<=100) {//true
+//			System.out.println(barath);
+//			barath=barath+2;//post increment
+//		}
+//		int barath=1;//2
+//		while(barath<=100) {//true
+//			System.out.println(barath);
+//			barath=barath*3;//post increment
+//		}
+//		float barath=30.747f ;//2
+//		while(barath<=100) {//true 
+//			System.out.println(barath);
+//			barath=barath/3;//post increment
+//		}
+		
+//		int barath=1;//2
+//		while(barath<=100) {//true
+//			System.out.println(barath);
+//			barath=barath+1;//post increment
+//		}
+		
+//		for(;;) {
+//			System.out.println("hi");
+//		}
+//		for(int barath=1;barath<=100;barath++) {
+//			System.out.println(barath);
+//		}
+//		int i =1;
+//		for(;i<=5;) {
+//			System.out.println("hi");
+//			i++;
+//		}
+		int i=11;
+		if(i<10) {
+			System.out.println("blue jeans");
+			System.out.println("black jeans");
+		}
+		
+	}
+
+}

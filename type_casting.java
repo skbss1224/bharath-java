a030c7ac7189a44c020958ac0b197ab800fc1f6b
skbss1224 <73568092+skbss1224@users.noreply.github.com>
2022-06-30 07:51:40 +0530

    type casting
    casting

---
+++ type_casting.java
@@ -0,0 +1,44 @@
+package bharath;
+
+public class type_casting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//type casting
+//		int x=10;
+//		byte y=(byte)x;
+//		System.out.println(y);
+		
+//		
+//		int i=100;
+//		long l=i;
+//		float f=i;
+//		System.out.println("int value:"+i);
+//		System.out.println("long value:"+l);
+//		System.out.println("float value:"+f);
+		
+//		double d=100.04;
+//		long l=(long)d;
+//		int i=(int)l;
+//		System.out.println("double value"+d);
+//		System.out.println("long value"+l);
+//		System.out.println("int value"+i);
+		
+//		byte b;
+//		int i=355;
+//		double d=425.150;
+//		b=(byte)i;
+//		System.out.println("conversation of int to byte:i="+i+"b="+b);
+//		System.out.println("**********************************");
+//		b=(byte)d;
+//		System.out.println("conversation of double to byte:d="+d+"b="+b);
+		
+		int n,i;
+		n=2;
+		for (i=1;i<=10;i++)
+		{
+			System.out.println(n+"*"+i+"="+n*i);
+		}
+	}
+
+}
